80c7aa99d51b9a41fa531ccb16f27e6741194099
Juan2579 <juandavidgualdronnavarro@gmail.com>
2022-11-01 17:59:01 -0500

    imperative states with useState

--- src/components/UseState/UseState.jsx
+++ src/components/UseState/UseState.jsx
@@ -5,13 +5,30 @@ const SECURITY_CODE = "paradigma"
 export const UseState = ({name}) => {
     const [state, setState] = useState({
         value: "",
-        error:false,
+        error: false,
         loading: false,
-        active: false,
+        deleted: false,
         confirmed: false,
     })
 
-    console.log(state.value)
+    const onConfirm = () => {
+        setState({
+            ...state,
+            error: false,
+            loading: false,
+            confirmed: true,
+        })
+    }
+    const onError = () => {
+        setState({
+            ...state,
+            error: true,
+            loading: false,
+        })
+    }
+    const onWrite = (newValue) => {
+        setState({...state, value: newValue})
+    }
 
     useEffect(() => {
         console.log("Empezando el efecto")
@@ -21,18 +38,9 @@ export const UseState = ({name}) => {
                 console.log("Haciendo la validacion")
 
                 if(state.value === SECURITY_CODE){
-                    setState({
-                        ...state,
-                        error: false,
-                        loading: false,
-                    })
-                    // setLoading(false)
+                    onConfirm()
                 }else{
-                    setState({
-                        ...state,
-                        error: true,
-                        loading: false,
-                    })
+                    onError()
                 }
     
                 console.log("Terminando la validacion")
@@ -41,24 +49,50 @@ export const UseState = ({name}) => {
 
         console.log("Terminando el efecto")
     }, [state.loading])
-    return (
-        <div className="bg-gray-600 text-white flex flex-col items-center justify-center gap-5 p-10">
-            <h2 className="text-xl font-bold">Eliminar {name}</h2>
-            <p className="text-gray-300">Por favor, escribe el codigo de seguridad</p>
+    
+    if(!state.deleted && !state.confirmed){
+        return (
+            <div className="bg-gray-600 text-white flex flex-col items-center justify-center gap-5 p-10">
+                <h2 className="text-xl font-bold">Eliminar {name}</h2>
+                <p className="text-gray-300">Por favor, escribe el codigo de seguridad</p>
+    
+                {state.error && (
+                    <p>Error: El codigo es incorrecto</p>
+                )}
+                {state.loading && (
+                    <p>Cargando...</p>
+                )}
+    
+                <input className="text-black" 
+                disabled={state.loading}
+                onChange={(event) => onWrite(event.target.value)}
+                value={state.value}
+                placeholder="Codigo de seguridad" 
+                type="text" />
+                <button className="bg-white text-black rounded-xl p-2 disabled:opacity-50" onClick={() => setState({...state, loading: true, error: false})} disabled={state.value == "" || state.loading}>Comprobar</button>
+            </div>
+        )
+    }else if(state.confirmed && !state.deleted){
+        return (
+            <div className="bg-gray-600 text-white flex flex-col items-center justify-center gap-5 p-10">
+                <h2 className="text-xl font-bold">Eliminar {name}</h2>
+                <p className="text-gray-300">¿Seguro que quieres eliminar UseState?</p>
 
-            {state.error && (
-                <p>Error: El codigo es incorrecto</p>
-            )}
-            {state.loading && (
-                <p>Cargando...</p>
-            )}
+                <div className="flex gap-3">
+                    <button className="bg-white text-black rounded-xl p-2" onClick={() => setState({...state, deleted: true})}>Si, eliminar</button>
+                    <button className="bg-white text-black rounded-xl p-2" onClick={() => setState({...state, value: "", confirmed: false})}>No, volver</button>
+                </div>
+            </div>
+        )
 
-            <input className="text-black" 
-            onChange={(event) => setState({...state, value: event.target.value})}
-            value={state.value}
-            placeholder="Codigo de seguridad" 
-            type="text" />
-            <button className="bg-white text-black rounded-xl p-2" onClick={() => setState({...state, loading: true, error: false})}>Comprobar</button>
-        </div>
-    )
+    }else {
+        return (
+            <div className="bg-gray-600 text-white flex flex-col items-center justify-center gap-5 p-10">
+
+                <h2 className="text-xl font-bold">{name} fue eliminado</h2>
+                <button className="bg-white text-black rounded-xl p-2" onClick={() => setState({value: "", deleted: false, confirmed: false,})}>Recuperar {name}</button>
+
+            </div>
+        )
+    }
 }
