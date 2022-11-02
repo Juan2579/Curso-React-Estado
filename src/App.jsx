1d97e8ed1e1a63deb4518dbb3f1dfca597a26fc8
Juan2579 <juandavidgualdronnavarro@gmail.com>
2022-11-02 14:49:13 -0500

    Added useReducer logic with reducer and reducerObject

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import { ClassState } from './components/ClassState/ClassState'
 import { UseState } from './components/UseState/UseState'
+import { UseReducer } from './useReducer'
 
 export const App = () => {
     return (
         <div className='bg-gray-700 flex flex-col justify-center items-center gap-10 py-5'>
             <UseState name="Use State" />
-            <ClassState name="Class State" />
+            <UseReducer name="Use Reducer" />
         </div>
     )
 }
--- src/useReducer.jsx
+++ src/useReducer.jsx
@@ -1,3 +1,83 @@
+import { useReducer, useEffect } from "react"
+
+const SECURITY_CODE = "paradigma"
+
+export const UseReducer = ({name}) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    useEffect(() => {
+        console.log("Empezando el efecto")
+
+        if(state.loading){
+            setTimeout(() => {
+                console.log("Haciendo la validacion")
+
+                if(state.value === SECURITY_CODE){
+                    dispatch({ type: "CONFIRM" })
+                }else{
+                    dispatch({ type: "ERROR" })
+                }
+    
+                console.log("Terminando la validacion")
+            }, 3000)
+        }
+
+        console.log("Terminando el efecto")
+    }, [state.loading])
+    
+    if(!state.deleted && !state.confirmed){
+        return (
+            <div className="bg-gray-600 text-white flex flex-col items-center justify-center gap-5 p-10">
+                <h2 className="text-xl font-bold">Eliminar {name}</h2>
+                <p className="text-gray-300">Por favor, escribe el codigo de seguridad</p>
+    
+                {state.error && (
+                    <p>Error: El codigo es incorrecto</p>
+                )}
+                {state.loading && (
+                    <p>Cargando...</p>
+                )}
+    
+                <input className="text-black" 
+                disabled={state.loading}
+                onChange={(event) => { dispatch({type: "WRITE", payload: event.target.value }) 
+                    // onWrite(event.target.value)
+                }}
+                value={state.value}
+                placeholder="Codigo de seguridad" 
+                type="text" />
+                <button 
+                className="bg-white text-black rounded-xl p-2 disabled:opacity-50" 
+                onClick={() => dispatch({ type: "CHECK" })} 
+                disabled={state.value == "" || state.loading}>Comprobar</button>
+            </div>
+        )
+    }else if(state.confirmed && !state.deleted){
+        return (
+            <div className="bg-gray-600 text-white flex flex-col items-center justify-center gap-5 p-10">
+                <h2 className="text-xl font-bold">Eliminar {name}</h2>
+                <p className="text-gray-300">¿Seguro que quieres eliminar UseState?</p>
+
+                <div className="flex gap-3">
+                    <button className="bg-white text-black rounded-xl p-2" onClick={() => dispatch({ type: "DELETE" })}>Si, eliminar</button>
+                    <button className="bg-white text-black rounded-xl p-2" onClick={() => dispatch({ type: "RESET" })}>No, volver</button>
+                </div>
+            </div>
+        )
+
+    }else {
+        return (
+            <div className="bg-gray-600 text-white flex flex-col items-center justify-center gap-5 p-10">
+
+                <h2 className="text-xl font-bold">{name} fue eliminado</h2>
+                <button className="bg-white text-black rounded-xl p-2" onClick={() => dispatch({ type: "RESET" })}>Recuperar {name}</button>
+
+            </div>
+        )
+    }
+}
+
+
 const initialState = {
     value: "",
     error: false,
@@ -6,64 +86,39 @@ const initialState = {
     confirmed: false,
 }
 
-// const reducer = (state, action) => {   
-// }
-const reducerIf = (state, action) => {   
-    if(action.type === "ERROR") {
-        return {
-            ...state,
-            error: true,
-            loading: false
-        }
-    }else if(action.type === "CHECK"){
-        return {
-            ...state,
-            loading: true,
-            error: false
-        }
-    }else{
-        return {
-            ...initialState
-        }
-    }
-}
 
-const reducerSwitch = (state, action) => {
-    switch (action.type) {
-        case "ERROR":
-            return {
-                ...state,
-                error: true,
-                loading: false
-            }
-            break;
-        case "CHECK":
-            return {
-                ...state,
-                error: false,
-                loading: true,
-            }
-            break;
-    
-        default:
-            return {
-                ...state
-            }
-            break;
-    }
-}
-
-const reducerObject = (state) => {
+const reducerObject = (state, payload) => {
     return {
+        "CONFIRM": {
+            ...state,
+            error: false,
+            loading: false,
+            confirmed: true,
+        },
         "ERROR": {
             ...state,
             error: true,
             loading: false
         },
+        "WRITE": {
+            ...state,
+            value: payload
+
+        },
         "CHECK": {
             ...state,
             error: false,
             loading: true
+        },
+        "DELETE": {
+            ...state,
+            deleted: true
+        },
+        "RESET": {
+            ...state,
+            value: "", 
+            deleted: false, 
+            confirmed: false,
         },
 
     }
@@ -71,10 +126,8 @@ const reducerObject = (state) => {
 
 const reducer = (state, action) =>{
     if(reducerObject(state)[action.type]){
-        return reducerObject(state)[action.type]
+        return reducerObject(state, action.payload)[action.type]
     }else{
         return state
     }
 }
-
-
